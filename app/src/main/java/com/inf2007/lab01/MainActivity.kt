91d659c484e0861cde3fca0ba4c8b7bea36ba04c
Bernard Lee <2957895l@student.gla.ac.uk>
2025-01-09 10:54:06 +0800

    Fix typo error; InF -> INF.

--- app/src/main/java/com/inf2007/lab01/MainActivity.kt
+++ app/src/main/java/com/inf2007/lab01/MainActivity.kt
@@ -95,7 +95,7 @@ fun UserInput(name: String, onNameChange: (String) -> Unit, modifier: Modifier =
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!, Welcome to InF2007!",
+        text = "Hello $name!, Welcome to INF2007!",
         modifier = Modifier
             .fillMaxWidth()
             .testTag("greetingMsg")
